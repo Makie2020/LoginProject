62913f467c520cae4450599fa7058009e9f42ae6
Marieke <69918471+Makie2020@users.noreply.github.com>
2024-02-14 15:56:56 +0100

    Adding routing to child components admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// Componentes
+// Components
 import { LoginComponent } from './components/login/login.component';
 import { SignInComponent } from './components/register/signin.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TableComponent } from './components/table/table.component';
 
 // Guards
 import { AuthGuard } from './utils/guards/auth.guard';
 import { AdminGuard } from './utils/guards/admin.guard';
 
+
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   { path: 'signIn', component: SignInComponent },
-  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard, AdminGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard, AdminGuard],    
+    children: [
+    {path: 'home', component: TableComponent},
+    {path: 'users', component: TableComponent},
+    {path: 'role', component: TableComponent},
+    {path: 'permission', component: TableComponent},
+] },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
 
