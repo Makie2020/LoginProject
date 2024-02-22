508af13b74eb0cfc567f534b290aeec2338355f7
Marieke <69918471+Makie2020@users.noreply.github.com>
2024-02-22 14:19:14 +0100

    Auto-generated commit

--- src/app/enum/permission.ts
+++
@@ -1,6 +0,0 @@
-export enum Permission {
-  Create = 1,
-  Update = 2,
-  Read = 3,
-  Delete = 4
-}
--- src/app/interfaces/userRole.ts
+++
@@ -1,7 +0,0 @@
-export interface IUserRole {
-  id: number,
-  user_id: number,
-  role_id: number,
-  createdAt: any,
-  updatedAt: any
-}
